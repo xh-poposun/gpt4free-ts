9e0183a9c3e0c923f67416f8d0204dd92225d0ae
xh-poposun <54991228+xh-poposun@users.noreply.github.com>
2023-06-29 22:51:36 +0800

    Update index.ts

--- model/index.ts
+++ model/index.ts
@@ -6,6 +6,7 @@ import {Phind} from "./phind";
 import {Vita} from "./vita";
 import {FakeOpen} from "./fakeopen";
 import {Better} from "./better";
+import {Forefrontnew} from "./forefront";
 
 export enum Site {
     // define new model here
@@ -16,6 +17,7 @@ export enum Site {
     Vita = 'vita',
     FakeOpen = 'fakeopen',
     Better = 'better',
+    Forefront = 'forefront',
 }
 
 export class ChatModelFactory {
@@ -37,6 +39,7 @@ export class ChatModelFactory {
         this.modelMap.set(Site.Vita, new Vita(this.options))
         this.modelMap.set(Site.FakeOpen, new FakeOpen(this.options))
         this.modelMap.set(Site.Better, new Better(this.options))
+         this.modelMap.set(Site.Forefront, new Forefrontnew(this.options))
     }
 
     get(model: Site): Chat | undefined {
